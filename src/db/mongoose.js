b56d7c5cad4a28ec74906ae0bca96d07e235e2aa
Akash Raaz Thakur <akashtha@in.ibm.com>
2022-07-17 13:24:36 +0530

    added dotenv

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 
 //connecting to DB using mongoose
+
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex:true
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config({silent:true});
 const express = require('express')
 const port = process.env.PORT
 require('./db/mongoose')
